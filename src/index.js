2b5e0ed8aa73032991e71b198cf6395c8990e827
Laxman2015 <lxmn2010@gmail.com>
2018-08-05 12:05:31 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function () {
+  return 'laxman react';
+}
+
+ReactDOM.render(
+  <App/>, document.getElementById('container'));
